99d68174f16a595fb12798e8adc2777ead119be9
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-10-22 20:36:46 +0100

    Update route.js

--- app/api/schedule/route.js
+++ app/api/schedule/route.js
@@ -1,1 +1,14 @@
+import { NextResponse } from 'next/server';
+import scheduleEmails from '@/utils/scheduleEmails';
 
+export async function GET() {
+  try {
+    // Call the scheduling function
+    await scheduleEmails();
+
+    return NextResponse.json({ message: 'Scheduling emails executed successfully.' }, { status: 200 });
+  } catch (error) {
+    console.error('Error running scheduling job:', error);
+    return NextResponse.json({ message: 'Error running scheduling job.' }, { status: 500 });
+  }
+}

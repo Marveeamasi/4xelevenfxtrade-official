44258de4800a455dfe376858a3d35d23ae55c48b
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-11-01 14:50:35 +0100

    Update route.js

--- app/api/schedule/route.js
+++ app/api/schedule/route.js
@@ -2,13 +2,14 @@ import { NextResponse } from 'next/server';
 import scheduleEmails from '@/utils/scheduleEmails';
 
 export async function GET() { 
-  export function GET(request: NextRequest) {
+  
   const authHeader = request.headers.get('authorization');
   if (authHeader !== `Bearer ${'LGi^X0266-obi*6~NBBo'}`) {
     return new Response('Unauthorized', {
       status: 401,
     });
   }
+  
   try {
     // Call the scheduling function
     await scheduleEmails();

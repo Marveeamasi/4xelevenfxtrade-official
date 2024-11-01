80b48b2e08bf9e75c1c9b2aab8c7fbfc57e6966e
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-11-01 16:44:35 +0100

    Update route.js

--- app/api/schedule/route.js
+++ app/api/schedule/route.js
@@ -1,13 +1,13 @@
 import scheduleEmails from '@/utils/scheduleEmails';
 
 export async function GET(request) {
+ try{
  const authHeader = request.headers.get('authorization');
   if (authHeader !== `Bearer ${'Vvee120!!!Vvee120!!!'}`) {
     return new Response('Unauthorized', {
       status: 401,
     });
   }
- try{
    await scheduleEmails();
    return new Response('Successfully run schedule', {status: 200});
  } catch (error){

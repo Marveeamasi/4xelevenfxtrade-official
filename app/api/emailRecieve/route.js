359b8e3b59f1101166a531bfca6a03f3d8ccc51d
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-11-28 11:00:00 +0100

    Update route.js

--- app/api/emailRecieve/route.js
+++ app/api/emailRecieve/route.js
@@ -16,8 +16,8 @@ export async function POST(req) {
         const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth:{
-    user: '4xelevenfxtrade@gmail.com',
-    pass: 'dqtg isjh yjdo fdyi',
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 

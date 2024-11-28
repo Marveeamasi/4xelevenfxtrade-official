9451ceb7e9cdd3bd20afd5b683514070f3ad30f3
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-11-28 11:02:27 +0100

    Update scheduleEmails.js

--- utils/scheduleEmails.js
+++ utils/scheduleEmails.js
@@ -6,8 +6,8 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth:{
-    user: '4xelevenfxtrade@gmail.com',
-    pass: 'dqtg isjh yjdo fdyi',
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 

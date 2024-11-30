c3d5a59f2373b9bbeccd41321fd35bf8a69b81fc
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-11-30 07:21:24 +0100

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
   return (
     <Slider {...settings}>
      <HeroSlide wi='w-[400px] max-sm:w-[200px]' animate='animate-pulse' img='bitcoinimg.png' action='Start Earning' title='Bitcoin currency, easy way to trade' desc='Start earning with our stable goal-oriented investment program'/>
-     <HeroSlide wi='w-[500px] max-sm:w-[200px]' animate='animate-bounce' img='rocket.png' action='Get Started' title='Invest, to grow your money' desc='The 4Elevenfxtrade is the most reliable and secure platform that makes trading, invseting and mining of crypto currency easy and safe.. and it supports variety of most popular digital currencies'/>
+     <HeroSlide wi='w-[500px] max-sm:w-[200px]' animate='animate-bounce' img='rocket.png' action='Get Started' title='Invest, to grow your money' desc='The 4xeleventrade is the most reliable and secure platform that makes trading, invseting and mining of crypto currency easy and safe.. and it supports variety of most popular digital currencies'/>
      <HeroSlide wi='w-[100px]' animate='animate-ping' img='thumbsup.png'  action='Invest Now' title='Bitcoin invest, easy and secure way' desc='Dont waste anymuch time after seeing this oppurtunity, start earning now'/>
     </Slider>
   )

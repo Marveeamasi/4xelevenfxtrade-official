63bfa136f139d0f72e46b3e91f258cb3ae34291e
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-11-30 07:56:20 +0100

    Update Sidebar.jsx

--- adminwidgets/Sidebar.jsx
+++ adminwidgets/Sidebar.jsx
@@ -15,7 +15,7 @@ export default function Sidebar({handleClick}) {
 
   return (
     <div className='flex sm:flex-col sm:sticky sm:top-0 sm:left-0 shadow-dark sm:shadow-dark-sm max-sm:justify-around max-sm:items-center sm:pl-5 sm:py-5 sm:w-[200px] w-full sm:h-screen h-auto bg-[#0a0c0c] max-sm:fixed bottom-0 z-[11]'>
-        <div className='flex items-center max-sm:hidden font-semibold mb-10 text-col'>4Elevenfxtrade's Admin</div>
+        <div className='flex items-center max-sm:hidden font-semibold mb-10 text-col'>4xeleventrade Admin</div>
         {navs.map((n) => (
             <div key={n.name} onClick={()=> handleClick(n.name)} className={`cursor-pointer flex max-sm:flex-col-reverse gap-2 items-center capitalize p-5 rounded-tl-[30px] rounded-bl-[30px]`}>
             <div className='max-sm:text-[10px] max-xsm:hidden'>{n.name}</div>

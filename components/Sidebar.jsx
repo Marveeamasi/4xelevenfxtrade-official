a1c9f19146ac964952cfdd2f50d6e918cc5ff768
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-11-30 07:46:31 +0100

    Update Sidebar.jsx

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -27,7 +27,7 @@ export default function Sidebar({title}) {
 
   return (
     <div className='flex sm:flex-col sm:sticky sm:top-0 sm:left-0 shadow-dark sm:shadow-dark-sm max-sm:justify-around max-sm:items-center sm:pl-5 sm:py-5 sm:w-[200px] w-full sm:h-screen h-auto bg-[#0a0c0c] max-sm:fixed bottom-0 z-[11]'>
-        <div className='flex items-center max-sm:hidden font-semibold mb-10 text-col'>4Elevenfxtrade</div>
+        <div className='flex items-center max-sm:hidden font-semibold mb-10 text-col'>4xeleventrade</div>
         {updatedNav.length>0 && updatedNav.map((n) => (
             <Link href={`/${n.name}`} key={n.name} className={`hover:opacity-[.8] flex max-sm:flex-col-reverse gap-2 items-center capitalize p-5 rounded-tl-[30px] rounded-bl-[30px] ${n.isActive? 'bg-[#00eaff] max-sm:bg-transparent text-[#00eaff] sm:text-black font-semibold' : 'sm:bg-transparent text-[#eee] font-light'}`}>
             <div className='max-sm:text-[10px] max-xsm:hidden '>{n.name}</div>
